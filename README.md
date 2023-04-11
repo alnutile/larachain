a157e9eeb74e863bfb0578d24389e3dac72a62ad
Alfred Nutile <365385+alnutile@users.noreply.github.com>
2023-04-10 20:49:04 -0400

    get the seeder going

--- README.md
+++ README.md
@@ -6,6 +6,22 @@ And just thinking how to make some of those flows work in a Laravel environment
 
 [https://github.com/hwchase17/langchain](https://github.com/hwchase17/langchain)
 
+## Setup
+Follow the Sail Laravel docs eg `sail up`
+
+Seed the user:
+
+Update your `.env` file:
+
+```dotenv
+ADMIN_EMAIL=foo@bar.com
+ADMIN_PASSWORD=foobaz
+```
+
+```bash
+sail artisan migrate
+sail artisan db:seed --class=UserSeeder
+```
 
 ## TODO
   * Embedding flow with vector 
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Team;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::whereEmail(env("ADMIN_EMAIL"))->first();
+        if(!$user) {
+            $user = new User();
+            $user->name = "Admin";
+            $user->email = env("ADMIN_EMAIL");
+            $user->password = bcrypt(env("ADMIN_PASSWORD"));
+            $user->save();
+        }
+
+        $team = new Team();
+        $team->personal_team = 1;
+        $team->user_id = $user->id;
+        $team->name = "Admin Team";
+        $team->save();
+
+        $user->current_team_id = $team->id;
+        $user->save();
+    }
+}
