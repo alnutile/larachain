669061f65bd373e46a8b20f1a871b18720e10687
Alfred Nutile <365385+alnutile@users.noreply.github.com>
2023-05-07 22:07:00 -0400

    working on source model and docs

--- app/Models/Source.php
+++ app/Models/Source.php
@@ -6,6 +6,17 @@ use App\Source\SourceTypeEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property string|null $description
+ * @property array|null $meta_data
+ * @property int $project_id
+ * @property int $order
+ * @property string $type
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ */
 class Source extends Model
 {
     use HasFactory;
