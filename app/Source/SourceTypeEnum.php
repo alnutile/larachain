ef5ba2b825428ea339f5210431b399c387138832
Alfred Nutile <365385+alnutile@users.noreply.github.com>
2023-05-18 12:32:14 -0400

    test thehelper

--- app/Source/SourceTypeEnum.php
+++ app/Source/SourceTypeEnum.php
@@ -2,31 +2,14 @@
 
 namespace App\Source;
 
+use App\Helpers\TypeTrait;
+
 /**
  * @see settings in config/larachain.php:3
  */
 enum SourceTypeEnum: string
 {
-    /**
-     * _ will just dispable the type until ready to show in the UI
-     */
-    case WebFile = 'web_file';
-    case Web = '_web';
-    case S3Dir = '_s3_directory';
+    use TypeTrait;
 
-    public function label(): string
-    {
-        return match ($this) {
-            static::WebFile => 'WebFile',
-            static::Web => 'Web',
-            static::S3Dir => 'S3Dir',
-        };
-    }
-
-    public static function toArray(): array
-    {
-        $result = [];
-        foreach (static::cases() as $case) {
-            if (! str($case->value)->startsWith('_')) {
     case WebFile = 'web_file';
 }
