ca1ea7df884a41167ab5baa8b16ed3aeb433dcd8
Alfred Nutile <365385+alnutile@users.noreply.github.com>
2023-05-28 15:57:02 -0400

    typo 🤦

---
+++ app/Console/Commands/Generators/TransformerGenerator.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands\Generators;
+
+use Illuminate\Console\Command;
+use Facades\App\Generators\Transformer\GeneratorRepository;
+
+class TransformerGenerator extends Command
+{
+    public $signature = 'larachain:transformer:create';
+
+    public $description = 'Generate the needed files for a new Transformer';
+
+    public function handle(): int
+    {
+        $name = $this->ask('What is the name of this Transformer type, example Html2Text');
+        $description = $this->ask('A description to let the user know what it is for');
+
+        $results = sprintf("Does this look right, the name is %s, the description is %s and requires settings '%s'.",
+        $name,
+        $description,
+        false);
+
+        if (! $this->confirm($results)) {
+            $this->error('Ok try again');
+            exit();
+        }
+
+        if (! $this->confirm('Lastly make sure your git status is clean so you can see what this command will output')) {
+            $this->error('Ok come back in a moment after you git add .');
+            exit();
+        }
+
+        $this->comment('Ok gonna go make the Generator one moment.');
+
+        GeneratorRepository::setup($name, $description, false)->run();
+
+        $this->comment('All done, check your git status');
+
+        return self::SUCCESS;
+    }
+}
