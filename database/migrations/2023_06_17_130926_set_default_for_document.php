0dd319c50d9886f11cd851117d61755fe1027237
Alfred Nutile <365385+alnutile@users.noreply.github.com>
2023-06-17 09:11:09 -0400

    big fix some odd default before for the document type as I try to reduce some of the types

---
+++ database/migrations/2023_06_17_130926_set_default_for_document.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Source\SourceEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('documents', function (Blueprint $table) {
+            $table->string("type")->default(SourceEnum::WebFile->value)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('documents', function (Blueprint $table) {
+            //
+        });
+    }
+};
